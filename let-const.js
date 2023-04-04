0275135112deffbeb2dd5901d185e0c69586c01c
alocke8181 <alocke_8181@yahoo.com>
2023-04-04 16:56:43 -0400

    added exercise

---
+++ let-const.js
@@ -0,0 +1,9 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+const PI = 3.14;
+
+//I don't know what the next coding section is asking for so here are a bunch of global variable declarations
+let globalLet = "I am a global let variable";
+const globalConst = "I am a global const variable";
+var globalVar = "I am a global var variable";
